b0cb3eab1ee942a6bfada69e16e5954d08d6e2c9
LAB1-PC03\ADMIN <nguyenduchoan.wru@gmail.com>
2023-04-01 10:54:55 +0700

    b4

---
+++ resources/views/upload/form.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master') <!-- Kế thừa master.blade.php -->
+@section('main')
+<h2>Form Upload</h2>
+<!-- Hiển thị flash message khi upload thành công -->
+@if(Session::has('success'))
+<div class="alert alert-success">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    {{Session::get('success')}}
+</div>
+@endif
+<form action="" method="POST" enctype="multipart/form-data">
+    @csrf
+    <div class="form-group">
+        <label for="">Chọn ảnh</label>
+        <input type="file" class="form-control" name="upload" placeholder="Ten danh mục">
+        @error('upload')
+        <small class="help-block text-danger">{{$message}}</small>
+        @enderror
+    </div>
+
+    <button type="submit" class="btn btn-primary">Upload</button>
+</form>
+@stop()
