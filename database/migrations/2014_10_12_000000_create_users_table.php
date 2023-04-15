dbe265b4e17991f2eeaae1362033c89e101ed5bb
Nguyen Xuan Hoang <hoangnx.hut@gmail.com>
2023-04-15 09:21:57 +0700

    'bdk1'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateCustomersTable extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
-            $table->increments('id'); // id int unsigned primary key auto_increment
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
             $table->string('name');
-            $table->string('email', 150)->unique();
-            $table->string('phone', 50)->unique();
-            $table->string('address');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateCustomersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('users');
     }
-}
+};
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\UploadController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,94 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('', [HomeController::class, 'index'])->name('home.index');
-// // route sẽ hiển thị form login
-// Route::get('login', [HomeController::class, 'login'])->name('home.login');
-
-// // route này validate dữ liệu khi submit form
-// Route::post('login', [HomeController::class, 'check_login']);
-
-// Route::get('/home/{id}', function($id) {
-//     return "Đây là sản phẩm thứ: " . $id;
-// })->where("id", '[0-9]+');
-
-// Route::prefix('category')->where("id", '[0-9]+')->group(function() {
-//     Route::get('/{id}', function($id) {
-//         return "Đây là trang category. Sản phẩm thứ: " . $id;
-//     });
-//     Route::get('/add', function() {
-//         return "Đây là trang thêm";
-//     });
-
-//     Route::get('/edit/{id}', function($id) {
-//         return "Đây là trang sửa. Sửa sản phẩm: " . $id;
-//     });
-
-//     Route::get('/delete/{id}', function($id) {
-//         return "Đây là trang xóa. Xóa sản phẩm: " . $id;
-//     });
-// });
-
-// Route::group([
-//     "prefix" => "category",
-//     "where" => [
-//         "id" => "[0-9]+"
-//     ]
-// ], function () {
-//     Route::get('/{id}', function ($id) {
-//         return "Đây là trang category. Sản phẩm thứ: " . $id;
-//     });
-//     Route::get('/add', function () {
-//         return "Đây là trang thêm";
-//     });
-
-
-//     Route::get('/edit/{id}', function ($id) {
-//         return "Đây là trang sửa. Sửa sản phẩm: " . $id;
-//     });
-
-
-//     Route::get('/delete/{id}', function ($id) {
-//         return "Đây là trang xóa. Xóa sản phẩm: " . $id;
-//     });
-// });
-
-// Route::group([
-//     "prefix" => "category"
-// ], function () {
-//     Route::get('/', function () {
-//         return [CategoryController::class, 'index'];
-//     });
-// });
-
-
-Route::get("/", function () {
-    return "Trang home!!!";
+Route::get('/', function () {
+    return view('welcome');
 });
-
-
-Route::get("/category", [CategoryController::class, "index"])->name('category.index');
-
-// GET categorry/create để hiển thị form nhập dữ liệu
-Route::get('category/create', [CategoryController::class, 'create'])->name('category.create');
-// POST categorry để nhận dữ liệu khi submit form
-Route::post('category', [CategoryController::class, 'store'])->name('category.createToDB');
-
-//DELETE category/{id} phương thức DELETE, khác với GET, hay POST
-Route::delete('categorry/{id}', [CategoryController::class, 'delete'])
-    ->where(["id" => "[0-9]+"])->name('category.delete');
-
-Route::get('categorry/{id}/edit', [CategoryController::class, 'edit'])->name('category.edit');
-
-Route::put('categorry/{id}', [CategoryController::class, 'update'])->name('category.update');
-
-// GET upload
-Route::get('upload',[UploadController::class, 'form'])->name('upload.form');
-// POST upload
-Route::post('upload',[UploadController::class, 'upload'])->name('upload.upload');
-
-
-Route::get('product', [ProductController::class, 'index'])->name('product.index');
-
-Route::get('product-join', [ProductController::class, 'indexJoin'])->name('product.index');
-
